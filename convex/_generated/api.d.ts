2d005562883a1eaffb979bd7852128137bc3feaf
p.kulkarni <p.kulkarni@axelor.com>
2025-03-14 14:26:15 +0530

    Add comments module with add and get comments functionality

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,6 +13,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as comment from "../comment.js";
 import type * as http from "../http.js";
 import type * as interviews from "../interviews.js";
 import type * as users from "../users.js";
@@ -26,6 +27,7 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  comment: typeof comment;
   http: typeof http;
   interviews: typeof interviews;
   users: typeof users;
